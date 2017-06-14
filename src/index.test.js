e531b9ad571c030595a211e3a5eacedeefe1dc22
Plazek <michael.plazek@hpe.com>
2017-06-14 14:37:05 -0600

    added addtional tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,47 @@
 import expect from 'expect';
 
-describe('Our first test', () => {
-  it('should pass', () => {
+describe('Our first suite', () => {
+  it('test 1.1', () => {
     expect(true).toEqual(true);
   });
 
-  it('shouldnt pass', () => {
+  it('test 1.2', () => {
+    expect(true).toEqual(false);
+  });
+});
+
+describe('Our second suite', () => {
+  it('test 2.1', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('test 2.2', () => {
+    expect(true).toEqual(false);
+  });
+
+  it('test 2.3', () => {
+    expect(true).toEqual(false);
+  });
+});
+
+describe('Our third suite', () => {
+  it('test 3.1', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('test 3.2', () => {
+    expect(true).toEqual(false);
+  });
+
+  it('test 3.3', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('test 3.4', () => {
+    expect(true).toEqual(false);
+  });
+
+  it('test 3.5', () => {
     expect(true).toEqual(false);
   });
 });
